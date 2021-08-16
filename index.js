be916918f68aa8a4dbe44275f6d0bdc88e7d3b91
Benis Tambe <will.jenifer@yahoo.com>
2021-08-16 13:21:50 -0400

    commit tree 14e255d3dee489b000a6ced88cd0aa3bb722ac4c

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your functions here
+
+function map(arr, callback){
+    const newArr =[];
+
+    for(let i = 0; i < arr.length; i++){
+        newArr.push(callback(arr[i]));
+    }
+    return newArr;
+}
+
+// function reduce(arr, callback){
+//     let returnVal;
+
+//     for(let i = 0; i < arr.length; i++){
+//         returnVal += callback(arr[i], startingPoint="");
+//     }
+//     return returnVal;
+// }
+
+//The SOL
+function reduce(src, callback, startingValue) {
+    let total;
+    if (startingValue) {
+      total = startingValue;
+      for (let i = 0; i < src.length; i++) {
+        total = callback(src[i], total);
+      }
+      return total;
+    } else {
+      total = src[0];
+      for (let i = 1; i < src.length; i++) {
+        total = callback(src[i], total);
+      }
+      return total;
+    }
+}
